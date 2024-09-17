ab3e463f632cc311344122c8e190900fb38f3544
Gustavo Teixeira <gustavo.teixeira2199@gmail.com>
2024-09-16 22:02:36 -0300

    Product insert, update, delete

--- backend/src/main/java/com/tavodin/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/tavodin/dscatalog/services/ProductService.java
@@ -2,11 +2,14 @@ package com.tavodin.dscatalog.services;
 
 import com.tavodin.dscatalog.dto.CategoryDTO;
 import com.tavodin.dscatalog.dto.ProductDTO;
+import com.tavodin.dscatalog.entities.Category;
 import com.tavodin.dscatalog.entities.Product;
+import com.tavodin.dscatalog.repositories.CategoryRepository;
 import com.tavodin.dscatalog.repositories.ProductRepository;
 import com.tavodin.dscatalog.services.exceptions.DataBaseException;
 import com.tavodin.dscatalog.services.exceptions.ResourceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -20,6 +23,9 @@ public class ProductService {
 
     @Autowired
     private ProductRepository repository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Transactional(readOnly = true)
     public Page<ProductDTO> findAll(Pageable pageable) {
@@ -37,7 +43,7 @@ public class ProductService {
     @Transactional
     public ProductDTO insert(ProductDTO dto) {
         Product entity = new Product();
-        //entity.setName(dto.getName());
+        copyDtoToEntity(dto, entity);
         entity = repository.save(entity);
         return new ProductDTO(entity);
     }
@@ -46,7 +52,7 @@ public class ProductService {
     public ProductDTO update(Long id, ProductDTO dto) {
         try {
             Product entity = repository.getReferenceById(id);
-            //entity.setName(dto.getName());
+            copyDtoToEntity(dto, entity);
             entity = repository.save(entity);
             return new ProductDTO(entity);
         } catch (EntityNotFoundException e) {
@@ -67,4 +73,17 @@ public class ProductService {
         }
     }
 
+    private void copyDtoToEntity(ProductDTO dto, Product entity) {
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgUrl(dto.getImgUrl());
+        entity.setDate(dto.getDate());
+
+        entity.getCategories().clear();
+        for(CategoryDTO catDto : dto.getCategories()) {
+            Category category = categoryRepository.getReferenceById(catDto.getId());
+            entity.getCategories().add(category);
+        }
+    }
 }
